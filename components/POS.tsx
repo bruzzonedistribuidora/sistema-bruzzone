6580f98c8a777fed1e23b99849b744d735610953
bruzzonedistribuidora <bruzzonedistribuidora@gmail.com>
2025-12-22 18:29:04 -0300

    Update POS.tsx

--- components/POS.tsx
+++ components/POS.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Lock } from 'lucide-react';
-import Dashboard from './components/Dashboard';
 import Inventory from './components/Inventory';
 import POS from './components/POS';
 import Remitos from './components/Remitos';
