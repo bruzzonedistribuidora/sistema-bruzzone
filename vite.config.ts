6ec9076d96179c1d2add122d9446bf2e8f8e3114
bruzzonedistribuidora <bruzzonedistribuidora@gmail.com>
2026-01-08 09:54:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Marcamos como externas las librerías que se cargan vía CDN en el index.html
-      // Usamos strings exactos y expresiones regulares para capturar sub-rutas (como react/jsx-runtime)
-      external: [
-        'react',
-        'react-dom',
-        'react/jsx-runtime',
-        'lucide-react',
-        '@google/genai',
-        'recharts',
-        /^react\/.*/,
-        /^react-dom\/.*/,
-      ],
-    }
-  },
   server: {
     port: 3000
   }
