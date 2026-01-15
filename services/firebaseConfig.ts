c2d7765c71725d94da72580980a98b8a29e53166
bruzzonedistribuidora <bruzzonedistribuidora@gmail.com>
2026-01-15 16:09:08 -0300

    Create firebaseConfig.ts

---
+++ services/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzBPwTL4idkBKgDLVwOW3TlqjIpkZLULA",
+  authDomain: "sistemagestionbruzzone.firebaseapp.com",
+  projectId: "sistemagestionbruzzone",
+  storageBucket: "sistemagestionbruzzone.firebasestorage.app",
+  messagingSenderId: "569643821205",
+  appId: "1:569643821205:web:72a6ba8eca5669c12f139c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
