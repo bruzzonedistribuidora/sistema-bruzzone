28ded4cd609d84a923ca1667defc2f693c5539ba
bruzzonedistribuidora <bruzzonedistribuidora@gmail.com>
2026-01-17 09:53:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Importamos el módulo path
+import { fileURLToPath } from 'url'; // Import fileURLToPath
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  }
+  },
+  resolve: {
+    alias: {
+      // Use import.meta.url for ESM compatibility
+      '@components': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './components'),
+    },
+  },
 });
